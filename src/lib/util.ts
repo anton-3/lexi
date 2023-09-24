96e610e6fc3ebe295df08c92bd23dabe1d5c3bc8
Anton Angeletti <angelettianton@gmail.com>
2023-09-24 03:47:15 -0500

    fix broken merge

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -20,7 +20,7 @@ async function gpt(prompt: string) {
   const gptResponse = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     messages: [{ role: 'user', content: prompt }],
-    temperature: 0.2,
+    temperature: 0.4,
     max_tokens: 500,
   })
   return gptResponse.choices[0].message.content
@@ -33,7 +33,7 @@ async function generateStory(wordList: string) {
   let wordString = ''
   words.forEach((word) => (wordString += `'${word}', `))
   wordString = wordString.slice(0, wordString.length - 2)
-  const prompt = `You will write sentences, with the goal of teaching children a new language by incorporating specific vocabulary words in your story. Your task is to write a short story that includes certain vocabulary words. 
+  const prompt = `You are a children's storybook author, with the goal of teaching children a new language by incorporating specific vocabulary words in your story. Your task is to write a short story that includes certain vocabulary words. 
 
 Your story must contain each of the following words: ${wordString}
 
@@ -48,8 +48,8 @@ You will follow the following instructions:
 7. You will design the sentences to form in a narrative structure, so that each sentence is related to each of the rest. Every sentence should connect to the one before it and the one after it.
 8. The sentences must contain each word in the word list at least once. You can use one or more listed words per sentence, but they all hve to be used at some point, and each sentence needs one listed word.
 9. The words can be used in any order, as long as they maintain a cohesive story from start to end.
-10. Do not use any pronouns such as "he, him, she, her, they, we," etc. Don't use pronouns in other languages either
-11. Make the sentences relatively simple, to the point where a new lanugage learner could understand.s`
+10. Do not use any pronouns such as "he, she, we, I, they".
+11. Make the sentences relatively simple, to the point where a new language learner could understand.`
 
   return await gpt(prompt)
 }
@@ -63,7 +63,7 @@ You are supposed to provide descriptions for an artist who will paint scenes of 
 Please delimit the scenes with the string '---' between each scene.
 
 Because each sentence is a new scene, you want to make sure subjects and objects that appear in more than one scene are painted the same way.
-Because of this, make sure you repeat the descriptions exactly for a subject or object that appears multiple times. This means that each time a noun appears, it will have the same description. If you first describe a man as "tall, wears ballcap, has beard", you will have to describe him as that after every sentence he is in
+Because of this, make sure you repeat the descriptions exactly for a subject or object that appears multiple times. THis means that each time a noun appears, it will have the same description. If you first describe a man as "tall, wears ballcap, has beard", you will have to describe him as that after every sentence he is in
 
 Here is an example of how you would format a description: if the sentence was "The woman walked outside", an appropriate modification would be "The woman walked outside", (the woman is tall with brown hair and is wearing a red dress)
 
@@ -86,11 +86,7 @@ You cannot change the descriptors for the same noun. once you come up with one, 
 Also, you will specify a setting after the descriptors in each sentence in the style of: setting: "sample setting". You will replace the "sample setting" with whatever you choose. The setting will be what fits best in context.
 You will only add content after the sentence.
 
-Here is an example of how you would format a description and setting together: if the sentence was "The woman walked outside", an appropriate modification would be "The woman walked outside", (the woman is tall with brown hair and is wearing a red dress) (Setting: The park outside a house)
-
-Also remember that if you see the pronoun "I", you have to describe it like any other noun each time
-
-Make sure to re-describe each noun as it was originally. if you would like to add in context description, do so, but ensure you always describe the physical features first`
+Here is an example of how you would format a description and setting together: if the sentence was "The woman walked outside", an appropriate modification would be "The woman walked outside", (the woman is tall with brown hair and is wearing a red dress) (Setting: The park outside a house)`
   return await gpt(prompt)
 }
 
