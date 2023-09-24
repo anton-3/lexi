ecffc2869140acc9ae7ba0c3573671240419676e
Thomas Waller <tswaller04@gmail.com>
2023-09-24 00:07:11 -0500

    put back original instructions

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -41,9 +41,13 @@ You will follow the following instructions:
 2. Each sentence MUST be written on its own line.
 3. You will write ${words.length}-${words.length + 2} sentences.
 4. You will write the sentences and ONLY the sentences.
-5. The story must contain each word in the word list at least once.
-6. The words can be used in any order, as long as they maintain a cohesive story from start to end.
-7. Do not use any pronouns such as "he, she, we, I, they".`
+5. You will include the word in the same form as written in the list. it will be the exact same spelling. it will be the exact same part f speech. you will completely avoid using similar words and alternate apellings. 
+6. You will use correct grammar and tense to mold sentences around the selected word
+7. You will design the sentences to form in a narrative structure, so that each sentence is related to each of the rest. Every sentence should connect to the one before it and the one after it.
+8. The sentences must contain each word in the word list at least once. You can use one or more listed words per sentence, but they all hve to be used at some point, and each sentence needs one listed word.
+9. The words can be used in any order, as long as they maintain a cohesive story from start to end.
+10. Do not use any pronouns such as "he, she, we, I, they".
+11. Make the sentences relatively simple, to the point where a new lanugage learner could understand.`
 
   return await gpt(prompt)
 }
